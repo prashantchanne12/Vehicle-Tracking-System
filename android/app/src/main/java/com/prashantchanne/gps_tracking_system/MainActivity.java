d6fae32d04686d49ed519f253884545afb39d992
prashantchanne12 <prashantchanne121@gmail.com>
2021-04-19 14:11:27 +0530

    project setup

---
+++ android/app/src/main/java/com/prashantchanne/gps_tracking_system/MainActivity.java
@@ -0,0 +1,6 @@
+package com.prashantchanne.gps_tracking_system;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vehicle Tracking System',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vehicle Tracking System'),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
+}
