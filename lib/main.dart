6048befb1a422aa0517df39a457acfa33d1eb334
prashantchanne12 <prashantchanne121@gmail.com>
2021-04-19 15:16:28 +0530

    fetching data from firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+final databaseReference = FirebaseDatabase.instance.reference();
 
 void main() {
   runApp(MyApp());
@@ -13,6 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        fontFamily: 'poppins',
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
@@ -20,7 +24,31 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var latitude = '';
+  var longitude = '';
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  void getData() {
+    databaseReference.once().then((DataSnapshot snapshot) {
+      print('Data : ${snapshot.value}');
+      setState(() {
+        latitude = '${snapshot.value['latitude']}';
+        longitude = '${snapshot.value['longitude']}';
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,7 +56,79 @@ class MyHomePage extends StatelessWidget {
         title: Text('Vehicle Tracking System'),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 25.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Card(
+                  child: Container(
+                    width: (MediaQuery.of(context).size.width - 50) / 2,
+                    padding: EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.transparent),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Latitude',
+                          style: TextStyle(
+                            fontSize: 22.0,
+                          ),
+                        ),
+                        SizedBox(height: 15.0),
+                        Text(
+                          latitude,
+                          style: TextStyle(
+                            fontSize: 26.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  child: Container(
+                    width: (MediaQuery.of(context).size.width - 50) / 2,
+                    padding: EdgeInsets.all(20.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.transparent),
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Longitude',
+                          style: TextStyle(
+                            fontSize: 22.0,
+                          ),
+                        ),
+                        SizedBox(height: 15.0),
+                        Text(
+                          longitude,
+                          style: TextStyle(
+                            fontSize: 26.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            RaisedButton(
+              child: Text('Get Data'),
+              onPressed: () {
+                getData();
+              },
+            )
+          ],
+        ),
+      ),
     );
   }
 }
